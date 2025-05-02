92a75c5b428c9672297041311df3d0b7cca1d6f4
khaledHamidi <engkhamidi@gmail.com>
2025-05-02 16:08:47 +0300

    Add project files.

---
+++ Visual Control Lab/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Visual_Control_Lab
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+            label1.Parent = pictureBox1;
+        }
+
+
+        #region الأحداث
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Lec02().Show();             // فتح نافذة محاضرة2 وعرضها
+        }
+
+
+        #endregion
+    }
+}
